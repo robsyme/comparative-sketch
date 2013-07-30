9ce475a50ea76eeee59878a25a2aa3c648a3ae69
Robert Syme <rob@rob-asus.(none)>
2013-07-30 16:07:52 +0800

    Pixel-perfect Mouseover highlighting.

--- js/canvasDotPlot.js
+++ js/canvasDotPlot.js
@@ -1,15 +1,19 @@
 width = 900;
 woff = 20;
 height = 500;
-hoff = 10;
+hoff = 20;
 
+var overlay = document.getElementById("dotPlotOverlay")
+var overlayCtx = overlay.getContext("2d");
 var canvas = document.getElementById("dotPlotCanvas")
 var ctx = canvas.getContext("2d");
 canvas.height = height + hoff * 2;
+overlay.height = height + hoff * 2;
 canvas.width = width + woff * 2;
+overlay.width = width + woff * 2;
 
 var delta = new DeltaParser("example-data/stago_lepto.delta");
-var delta = new DeltaParser("example-data/yeasts.delta");
+//var delta = new DeltaParser("example-data/yeasts.delta");
 var refSeqs = delta.refs().sort(function(a,b) {return b.length - a.length;});
 var qrySeqs = delta.qrys().sort(function(a,b) {return a.length - b.length;});
 
@@ -89,13 +93,63 @@ function getCursorPosition(e) {
   return {x:x,y:y};
 }
 
-function mouseTest(e) {
-  var mousePos = getCursorPosition(e);
-  console.log(mousePos);
+function getRefName(xPos) {
+  var refName;
+  var refStart;
+  if(xPos < 0 || xPos > cumulativeRefLength * refScaleFactor) {
+    return ;
+  }
+  for(var name in refOffSets) {
+    var oldName = refName;
+    var oldStart = refStart;
+    refName = name;
+    refStart = refOffSets[name] * refScaleFactor;
+    if(refStart > xPos) {
+      return {name: oldName, x: oldStart, width: refStart - oldStart};
+    }
+  }
+  return {name: refName, x: refStart, width: cumulativeRefLength * refScaleFactor - refStart};
+}
+function getQryName(yPos) {
+  var qryName;
+  var qryStart;
+  var oldName;
+  var oldStart = 0;
+  if(yPos < 0 || yPos > cumulativeQryLength * qryScaleFactor) {
+    return ;
+  }
+  for(var name in qryOffSets) {
+    oldName = qryName;
+    oldStart = qryStart;
+    qryName = name;
+    qryStart = qryOffSets[name] * qryScaleFactor;
+    if(qryStart > yPos) {
+      return {name: oldName, y: oldStart, height: qryStart - oldStart};
+    }
+  }
+  return {name: qryName, y: qryStart, height: cumulativeQryLength * qryScaleFactor - qryStart};
 }
 
+
+function mouseTest(e) {
+  var mousePos = getCursorPosition(e);
+  var ref = getRefName(mousePos.x);
+  var qry = getQryName(mousePos.y);
+  overlay.width = width + woff * 2;
+  if(ref && qry) {
+    overlayCtx.fillStyle = "rgba(70,130,80,0.05)";
+    overlayCtx.strokeStyle = "rgba(0,0,0,0.5)";
+    overlayCtx.lineWidth = 1;
+    var x = Math.floor(ref.x + woff + 0.5) + 0.5;
+    var y = Math.ceil(qry.y + hoff + 0.5) - 0.5;
+    var w = Math.floor(ref.width);
+    var h = Math.floor(qry.height);
+    overlayCtx.fillRect(x,y,w,h);
+    overlayCtx.strokeRect(x,y,w,h);
+  }
+}
+    
 clipPaths(ctx);
 drawGrid(ctx);
 drawHits(ctx);
-
 canvas.addEventListener("mousemove", mouseTest, false);
